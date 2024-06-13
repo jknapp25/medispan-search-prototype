5f28ce9db8923be7aa4a0e315de39644e9314dd9
jknapp25 <joshknappp@gmail.com>
2024-06-13 16:02:22 -0700

    add strength filter

--- app/search-3/page.jsx
+++ app/search-3/page.jsx
@@ -6,6 +6,8 @@ import { useRouter } from "next/navigation";
 import { boldSubstring } from "../utils";
 
 function parseDosage(dosage) {
+  if (!dosage) return { value: 0, unit: "" };
+
   const match = dosage.match(/(\d+)\s*(MG|MG\/ML|MG\/5ML)/);
   if (match) {
     return {
@@ -13,7 +15,7 @@ function parseDosage(dosage) {
       unit: match[2],
     };
   }
-  return null;
+  return { value: 0, unit: "" };
 }
 
 export default function Home() {
@@ -23,7 +25,9 @@ export default function Home() {
   const [concepts, setConcepts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [mediSpanId, setMediSpanId] = useState(null);
+  const [selectedConcept, setSelectedConcept] = useState(null);
   const [strengths, setStrengths] = useState([]);
+  const [selectedStrength, setSelectedStrength] = useState(null);
   const [showMenu, setShowMenu] = useState(false);
 
   const fetchConcepts = async (query) => {
@@ -44,13 +48,16 @@ export default function Home() {
   useEffect(() => {
     setLoading(true);
 
-    if (search.length > 2) {
+    if (
+      search.length > 2 &&
+      (!selectedConcept || search !== selectedConcept.name)
+    ) {
       debouncedFetchConcepts(search);
     } else {
       setConcepts([]);
       setLoading(false);
     }
-  }, [search, debouncedFetchConcepts]);
+  }, [search, debouncedFetchConcepts, selectedConcept]);
 
   useEffect(() => {
     return () => {
@@ -64,7 +71,6 @@ export default function Home() {
     );
     const data = await response.json();
     setStrengths(data?.results || []);
-    // setLoading(false);
   };
 
   useEffect(() => {
@@ -73,7 +79,12 @@ export default function Home() {
     }
   }, [mediSpanId]);
 
-  // Sorting function
+  useEffect(() => {
+    if (strengths.length === 1) {
+      setSelectedStrength(strengths[0]);
+    }
+  }, [strengths]);
+
   const sorted = strengths.sort((a, b) => {
     const aParsed = parseDosage(a);
     const bParsed = parseDosage(b);
@@ -81,7 +92,6 @@ export default function Home() {
     if (aParsed.value !== bParsed.value) {
       return aParsed.value - bParsed.value;
     } else {
-      // If values are equal, sort by unit
       return aParsed.unit.localeCompare(bParsed.unit);
     }
   });
@@ -97,69 +107,18 @@ export default function Home() {
         </div>
       </div>
       <div className="col-span-2">
-        <div className="w-full">
-          <div className="inline-block">
-            <input
-              type="text"
-              onChange={(e) => setSearch(e.target.value)}
-              value={search}
-              placeholder="Start typing..."
-              className={`mb-2 inline-block w-full ${
-                strengths?.length > 0 ? "rounded-l-lg" : "rounded-lg"
-              } border-0 py-4 text-lg text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:leading-6 px-5`}
-            />
-          </div>
-          {strengths?.length > 0 ? (
-            <div className="inline-block">
-              <button
-                type="button"
-                class="inline-flex justify-center rounded-r-md shadow-md bg-white px-5 py-4 text-lg text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                id="menu-button"
-                aria-expanded={showMenu}
-                aria-haspopup={showMenu}
-                onClick={() => setShowMenu(!showMenu)}
-              >
-                Strength
-                <svg
-                  class="-mr-1 ml-2 h-5 w-5 text-gray-400"
-                  viewBox="0 -5 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </button>
-              {showMenu ? (
-                <div
-                  class=" right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                  role="menu"
-                  aria-orientation="vertical"
-                  aria-labelledby="menu-button"
-                  tabindex="-1"
-                >
-                  <div class="py-1" role="none">
-                    {/* <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" --> */}
-                    {sorted.map((s, i) => (
-                      <a
-                        href="#"
-                        class="block px-4 py-2 text-sm text-gray-700"
-                        role="menuitem"
-                        tabindex="-1"
-                        id={`menu-item-${i}`}
-                      >
-                        {s}
-                      </a>
-                    ))}
-                  </div>
-                </div>
-              ) : null}
-            </div>
-          ) : null}
-        </div>
+        <input
+          type="text"
+          onChange={(e) => {
+            setSearch(e.target.value);
+            setSelectedConcept(null);
+            setSelectedStrength(null);
+          }}
+          value={search}
+          placeholder="Start typing..."
+          className={`mb-2 block w-full rounded-lg border-0 py-4 text-lg text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:leading-6 px-5`}
+        />
+
         {loading ? (
           <div className="mb-4 block w-full rounded-lg border-0 py-4 text-gray-900 shadow-md px-5 ring-1 ring-inset ring-gray-300">
             <div className="text-sm text-gray-400">Loading...</div>
@@ -171,11 +130,14 @@ export default function Home() {
         concepts.length === 0 ? (
           <div className="mb-4 block w-full rounded-lg border-0 py-4 text-gray-900 shadow-md px-5 ring-1 ring-inset ring-gray-300">
             <div className="text-sm text-gray-400">
-              Enter atleast 3 characters
+              Enter at least 3 characters
             </div>
           </div>
         ) : null}
-        {!loading && search.length > 2 && concepts.length === 0 ? (
+        {!loading &&
+        search.length > 2 &&
+        concepts.length === 0 &&
+        !selectedConcept ? (
           <div className="mb-4 block w-full rounded-lg border-0 py-4 text-gray-900 shadow-md px-5 ring-1 ring-inset ring-gray-300">
             <div className="text-sm text-gray-400">
               No results, try less specific
@@ -183,17 +145,19 @@ export default function Home() {
           </div>
         ) : null}
         {!loading && search.length > 0 && concepts.length > 0 ? (
-          <div className="mb-4 block w-full rounded-lg border-0 py-4 text-gray-900 shadow-md px-5 ring-1 ring-inset ring-gray-300">
+          <div className="relative mb-4 block w-full rounded-lg border-0 py-4 text-gray-900 shadow-md px-5 ring-1 ring-inset ring-gray-300">
             <ul className="divide-y">
               {concepts.map((c, i) => {
                 const boldedContent = boldSubstring(c.name, search);
                 return (
                   <li
-                    key={c}
-                    className={`py-2 cursor-pointer hover:bg-gray-50`}
+                    key={i}
+                    className="py-2 cursor-pointer hover:bg-gray-50"
                     onClick={() => {
                       setMediSpanId(c.mediSpanId);
-                      setSearch("");
+                      setSearch(c.name);
+                      setSelectedConcept(c);
+                      setConcepts([]);
                     }}
                   >
                     <div className="text-sm">{boldedContent}</div>
@@ -203,8 +167,73 @@ export default function Home() {
             </ul>
           </div>
         ) : null}
-        {/* MediSpanId: {mediSpanId}
-        Strengths: {strengths.map((s) => s)} */}
+
+        {selectedConcept ? (
+          <div className="mt-4 mb-1">
+            Medication: <b>{selectedConcept?.name}</b>
+          </div>
+        ) : null}
+
+        {strengths?.length > 0 && selectedConcept && (
+          <div className="inline-block relative mt-2">
+            Strength:{" "}
+            {selectedStrength ? (
+              <b>{selectedStrength}</b>
+            ) : (
+              <button
+                type="button"
+                className="inline-flex justify-center rounded-md shadow-md bg-white px-2 py-1 text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                id="menu-button"
+                aria-expanded={showMenu}
+                aria-haspopup={showMenu}
+                onClick={() => setShowMenu(!showMenu)}
+              >
+                Select
+                <svg
+                  className="-mr-1 ml-2 h-5 w-5 text-gray-400"
+                  viewBox="0 -5 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+            )}
+            {showMenu && strengths.length > 1 && (
+              <div
+                className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="menu-button"
+                tabIndex="-1"
+              >
+                <div className="py-1" role="none">
+                  {sorted.map((s, i) => (
+                    <a
+                      key={i}
+                      href="#"
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-50"
+                      role="menuitem"
+                      tabIndex="-1"
+                      id={`menu-item-${i}`}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setSelectedStrength(s);
+                        setShowMenu(false);
+                      }}
+                    >
+                      {s}
+                    </a>
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+        )}
       </div>
       <div className="col-span-1"></div>
     </div>
