fbf94082aa1909624840e10a9ce5580ec782a793
jknapp25 <joshknappp@gmail.com>
2024-06-14 09:52:41 -0700

    copy

--- app/page.jsx
+++ app/page.jsx
@@ -63,7 +63,7 @@ export default function Home() {
                 target={`_blank`}
                 className="text-blue-500 underline"
               >
-                Beep boop
+                Is this what you're searching for?
               </a>
             </div>
           ) : null}
